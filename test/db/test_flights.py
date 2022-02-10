1361b849ff86e572867697f8585e1c0d8aff91a4
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-10 15:34:37 +0200

    implement test for database's flight table

---
+++ test/db/test_flights.py
@@ -0,0 +1,37 @@
+from test_config import *
+from test_planes import get_plane
+from flights import * # refers to ../../db flights.py
+
+
+def get_flight():
+    plane = get_plane()
+    flight = Flight(
+            flight_datetime=datetime.datetime.now(),
+            flight_duration=123,
+            origin='NYK',
+            destination='CND',
+            updated_on=datetime.datetime.now(),
+            full=False
+            )
+    flight.plane = plane
+    return flight, plane
+
+
+def test_create_instance(db_handle):
+    '''
+    Tests that we can create one instance of the flight model and save it to the
+    database using valid values for all columns.
+    '''
+    flight, plane = get_flight()
+    db_handle.session.add(flight)
+    db_handle.session.commit()
+
+    # check that the flight is in db
+    assert Flight.query.count() == 1
+
+    # check that the flight's plane is correct
+    db_flight = Flight.query.first()
+    assert db_flight.plane == plane
+
+
+
