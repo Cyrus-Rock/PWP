c8c7a4a818f67b0b5d72570e473123a90e557c0a
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-10 15:35:25 +0200

    implement database's flight table

---
+++ db/flights.py
@@ -0,0 +1,15 @@
+from config import db
+from planes import Plane
+
+# db is created by the importing module
+class Flight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String(20), nullable=False)
+    destination = db.Column(db.String(20), nullable=False)
+    updated_on = db.Column(db.DateTime, nullable=False)
+    flight_datetime = db.Column(db.DateTime, nullable=False)
+    flight_duration = db.Column(db.Float, nullable=False)
+    full = db.Column(db.Boolean, nullable=False)
+    plane_id = db.Column(db.Integer, db.ForeignKey("plane.id", ondelete="SET NULL"))
+
+    plane = db.relationship('Plane', back_populates='flight', uselist=False)
