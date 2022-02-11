a09f1cf6cbc77cbf315baeabbcfdc5876c4c5cbe
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-11 08:59:20 +0200

    implement database's client table

---
+++ db/clients.py
@@ -0,0 +1,10 @@
+from config import db
+
+# db is created by the importing module
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_on = db.Column(db.DateTime, nullable=False)
+    token = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    surename = db.Column(db.String(80), nullable=False)
+
