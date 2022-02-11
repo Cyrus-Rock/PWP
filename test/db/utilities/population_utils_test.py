b5a9a3c9eb5222dcc09e001046178b90af4c17c6
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-11 15:02:25 +0200

    test the flight's table population

--- test/db/utilities/population_utils_test.py
+++ test/db/utilities/population_utils_test.py
@@ -5,6 +5,7 @@ sys.path.append('../../db/utilities/')
 from population_utils import Populate # refers to ../../db/utilities
 from planes import Plane # refers to ../../db
 from seats import Seat # refers to ../../db
+from flights import Flight # refers to ../../db
 
 
 def test_populate_plane(db_handle):
@@ -22,3 +23,12 @@ def test_populate_seat(db_handle):
     Populate.plane(db_handle)
     Populate.seat(db_handle)
     assert Seat.query.count() == 30
+
+
+def test_populate_flight(db_handle):
+    '''
+    Tests that Populate.flight creates 5 default values in db.
+    '''
+    Populate.plane(db_handle)
+    Populate.flight(db_handle)
+    assert Flight.query.count() == 5
