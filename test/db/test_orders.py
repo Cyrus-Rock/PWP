403cf6cc4f27e6d424d98751f39ae697d709ffc2
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-11 09:51:08 +0200

    implement test for database's order table

---
+++ test/db/test_orders.py
@@ -0,0 +1,27 @@
+from test_config import *
+from test_offers import get_offer
+from orders import * # refers to ../../db orders.py
+
+
+def get_order():
+    offer = get_offer()
+    order = Order(
+            created_on=datetime.datetime.now()
+            )
+    offer.order = order
+    order.offer = offer
+    return order
+
+
+def test_create_instance(db_handle):
+    '''
+    Tests that we can create one instance of the order table and save it to the
+    database using valid values for all columns.
+    '''
+    order = get_order()
+    db_handle.session.add(order)
+    db_handle.session.commit()
+
+    # check that the order is in db
+    assert Order.query.count() == 1
+
