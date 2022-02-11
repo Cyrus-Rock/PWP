16c116cc47d31cebd65be62f9fc391a0e68a10a4
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-11 13:35:20 +0200

    implement a test for the seat's table population

--- test/db/utilities/population_utils_test.py
+++ test/db/utilities/population_utils_test.py
@@ -4,6 +4,7 @@ from test_config import * # referes to ../
 sys.path.append('../../db/utilities/')
 from population_utils import Populate # refers to ../../db/utilities
 from planes import Plane # refers to ../../db
+from seats import Seat # refers to ../../db
 
 
 def test_populate_plane(db_handle):
@@ -12,3 +13,12 @@ def test_populate_plane(db_handle):
     '''
     Populate.plane(db_handle)
     assert Plane.query.count() == 15
+
+
+def test_populate_seat(db_handle):
+    '''
+    Tests that Populate.seat creates 30 default values in db.
+    '''
+    Populate.plane(db_handle)
+    Populate.seat(db_handle)
+    assert Seat.query.count() == 30
