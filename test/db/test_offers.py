80e2aef1942966d9a43649407ff69d25bde44938
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-11 09:27:18 +0200

    implement test for database's offer table

---
+++ test/db/test_offers.py
@@ -0,0 +1,31 @@
+from test_config import *
+from test_clients import get_client
+from test_flights import get_flight
+from offers import * # refers to ../../db offers.py
+
+
+def get_offer():
+    client = get_client()
+    flight, _plane = get_flight()
+    offer = Offer(
+            valid_until=datetime.datetime.now()
+            )
+    offer.client = client
+    client.offers.append(offer)
+    offer.flight = flight
+    flight.offers.append(offer)
+    return offer
+
+
+def test_create_instance(db_handle):
+    '''
+    Tests that we can create one instance of the offer table and save it to the
+    database using valid values for all columns.
+    '''
+    offer = get_offer()
+    db_handle.session.add(offer)
+    db_handle.session.commit()
+
+    # check that the seat is in db
+    assert Offer.query.count() == 1
+
