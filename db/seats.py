5ec1bb767ed5b19bebfd0d9a8afa27039e6da259
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:20:09 +0200

    provide documentation for the seat table in the database

--- db/seats.py
+++ db/seats.py
@@ -2,6 +2,20 @@ from config import db
 
 # db is created by the importing module
 class Seat(db.Model):
+    '''
+    This table holds the information for each plane's seats.
+
+    The following describe each field of the table:
+    
+    `type` determines the type of seats for each plane. For example, business
+           and economic classes.
+
+    `capacity` determines the number of seats available for each class. For
+               instance, 20 seats for economic and 10 for business classes.
+
+    `plane_id` references the plane for this seat attributes. The relationship
+               is 1:N for plane:seat.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(15), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
