f167764940d39a31ef219ffe1a7cd087c7d8f63c
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:45:37 +0200

    provide documentation for the order table in the database

--- db/orders.py
+++ db/orders.py
@@ -2,6 +2,15 @@ from config import db
 
 # db is created by the importing module
 class Order(db.Model):
+    '''
+    This table is used to record the clients' orders.
+
+    The following describes each field of the table:
+
+    `offer_id` references the offer which was accepted by the client.
+
+    `created_on` is the time stamp for the creation of the order.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     created_on = db.Column(db.DateTime, nullable=False)
     offer_id = db.Column(db.Integer, db.ForeignKey("offer.id", ondelete="SET NULL"))
