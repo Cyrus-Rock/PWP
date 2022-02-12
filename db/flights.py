5d5341717cdf8243b3002efe3e2b248b61e16c4f
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:02:26 +0200

    provide documentation for the flight table in the database

--- db/flights.py
+++ db/flights.py
@@ -2,6 +2,31 @@ from config import db
 
 # db is created by the importing module
 class Flight(db.Model):
+    '''
+    This table has information about the date of flight, which plane is used
+    for that flight, and the duration of flight along with the origin and
+    designation of the flight.
+
+    The following describe each field of the table:
+
+    `flight_datetime` determines the time and date of the flight.
+
+    `plane_id` references the plane that carries the passengers for this 
+               flight. The relationship is 1:1 for flight:plane.
+
+    `flight_duration` determines the time required for the plane to fly from
+                      origin to destination.
+
+    `origin`    is an abbreviated form for the origin of the airport.
+
+    `destination` is an abbreviated form for the destination of the airport.
+
+    `updated_on` is the time stamp for the last information update regarding 
+                 this flight.
+
+    `full` determines whether this flight still has a vacant seat for another
+           passenger to reserve the flight.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     origin = db.Column(db.String(20), nullable=False)
     destination = db.Column(db.String(20), nullable=False)
