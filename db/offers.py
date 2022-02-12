bdf52f3749ca30a141b08aed7111ebfaf8fecb4f
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:38:28 +0200

    provide documentation for the offer table in the database

--- db/offers.py
+++ db/offers.py
@@ -2,6 +2,17 @@ from config import db
 
 # db is created by the importing module
 class Offer(db.Model):
+    '''
+    This table is used to offer flights based on the client's criteria.
+
+    The following describes each field of the table:
+
+    `client_id` references the client who requested offers.
+
+    `valid_until` determines the time for validity of this offer.
+
+    `filght_id` references the flight for which this offer exists.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     valid_until = db.Column(db.DateTime, nullable=False)
     client_id = db.Column(db.Integer, db.ForeignKey("client.id", ondelete="SET NULL"))
