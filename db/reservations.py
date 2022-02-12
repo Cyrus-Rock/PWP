0bd06f17ca70b1b6ae8aabaf04c22ba7b5f901fe
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:28:41 +0200

    provide documentation for the reservation table in the database

--- db/reservations.py
+++ db/reservations.py
@@ -2,6 +2,15 @@ from config import db
 
 # db is created by the importing module
 class Reservation(db.Model):
+    '''
+    This table keeps track of all the reservations made by the clients.
+
+    The following describe each field of the table:
+    
+    `seat_number` determines the seat number for the client's choice. Its
+                  relationship is 1:N for order:reservation. This is because
+                  a client can reserve more than one seat.
+    '''
     id = db.Column(db.Integer,
             db.ForeignKey("order.id", ondelete="SET NULL"),
             primary_key=True)
