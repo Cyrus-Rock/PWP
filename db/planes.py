b8b9705dcbf2afeb8b59347bbdffebd02537a983
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:14:18 +0200

    provide documentation for the plane table in the database

--- db/planes.py
+++ db/planes.py
@@ -2,6 +2,18 @@ from config import db
 
 # db is created by the importing module
 class Plane(db.Model):
+    '''
+    This table holds the information related to planes in airports.
+
+    The following describe each field of the table:
+
+    `plane` is the name for this plane.
+
+    `current_location` determines the last location reported for this plane.
+
+    `updated_on` is the time stamp for the last information update regarding
+                 this flight.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     current_location = db.Column(db.String(20), nullable=False)
