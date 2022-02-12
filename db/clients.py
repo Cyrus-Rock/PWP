50a9605e84193bc93e0a49b7ad1b29a422f6b063
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-12 10:23:00 +0200

    provide documentation for the client table in the database

--- db/clients.py
+++ db/clients.py
@@ -2,6 +2,17 @@ from config import db
 
 # db is created by the importing module
 class Client(db.Model):
+    '''
+    This table holds the information related to the clients of the API.
+
+    The following describe each field of the table:
+
+    `token` is the token that is used by the client to make requests.
+
+    `created_on` is the timestamp for the creation time of the token.
+
+    `name` reperesents the name of the client.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     created_on = db.Column(db.DateTime, nullable=False)
     token = db.Column(db.String(255), nullable=False)
