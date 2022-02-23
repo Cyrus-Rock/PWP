61774063480dcb33436ba73e6c2fb6c1e06bc45b
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-23 17:30:19 +0200

    implement GET, DELETE, POST methods for seat resource

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from db.reservations import Reservation
 from db.config import * 
 import src.resources.clients
 import src.resources.planes
+import src.resources.seats
 import src.resources.converters.client_converter
 import src.resources.converters.plane_converter
+import src.resources.converters.seat_converter
 import src.resources.exception_handlers
 
 
@@ -19,6 +21,7 @@ src.resources.exception_handlers.Handle.not_found(app)
 
 app.url_map.converters['ClientConverter'] = src.resources.converters.client_converter.ClientConverter
 app.url_map.converters['PlaneConverter'] = src.resources.converters.plane_converter.PlaneConverter
+app.url_map.converters['SeatConverter'] = src.resources.converters.seat_converter.SeatConverter
 
 
 api.add_resource(src.resources.clients.Client, '/api/clients/<ClientConverter:client>/') # `client` is client's token; handles GET and DELETE
@@ -27,3 +30,8 @@ api.add_resource(src.resources.clients.ClientItem, '/api/clients/') # handles PO
 
 api.add_resource(src.resources.planes.Plane, '/api/planes/<PlaneConverter:plane>/') # `plane` is plane's id; handles GET and DELETE
 api.add_resource(src.resources.planes.PlaneItem, '/api/planes/') # handles POST
+
+
+
+api.add_resource(src.resources.seats.Seat, '/api/seats/<SeatConverter:seats>/') # `seats` is plane's id; handles GET and DELETE
+api.add_resource(src.resources.seats.SeatItem, '/api/seats/') # handles POST
---
+++ src/resources/seats.py
@@ -0,0 +1,49 @@
+from src.resources.config import *
+import db.seats
+
+
+class Seat(flask_restful.Resource):
+
+    def get(s, seats):
+        result = {
+                'plane_name': seats[0].plane.name,
+                'capacity': {s.type:s.capacity for s in seats},
+                'updated_on': str(seats[0].plane.updated_on)
+        }
+        return result, 200
+
+    def delete(s, seats):
+        for s in seats:
+                db.config.db.session.delete(s)
+        db.config.db.session.commit()
+        return flask.Response(status=200)
+
+
+class SeatItem(flask_restful.Resource):
+    def post(s):
+        if flask.request.content_type != 'application/json':
+            return "Request content type must be JSON", 415
+        if flask.request.method != 'POST':
+            return "POST method required", 405
+        try:
+            plane_id = flask.request.json['plane_id']
+            cpcty = flask.request.json['capacity']
+            seats = {k:cpcty[k] for k in cpcty}
+            updated_on = datetime.datetime.fromisoformat(flask.request.json['updated_on'])
+            
+            for type in seats:
+                seat = db.seats.Seat(
+                        plane_id=plane_id,
+                        capacity=seats[type],
+                        type=type)
+                db.config.db.session.add(seat)
+            db.config.db.session.commit()
+        except KeyError:
+            return "Incomplete request - missing fields", 400
+        except TypeError:
+            return "One or some of the provided types is not right", 401
+        return flask.Response(
+                headers={'location': db.config.api.url_for(Seat,
+                seats=plane_id)},
+                status=201)
+
