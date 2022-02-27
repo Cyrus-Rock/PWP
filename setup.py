3ec8f1342216046beac0bdf213dc85c8b1fb6f62
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-02-27 11:36:40 +0200

    improve setup

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='programmable_web_project', version='1.0', packages=find_packages())
+req = 'requirements.txt'
+
+with open(req, 'r') as f:
+    setup(name='programmable_web_project',
+        version='1.0',
+        packages=find_packages(),
+        install_requires=list(map(str.strip, f.readlines()))
+        )
