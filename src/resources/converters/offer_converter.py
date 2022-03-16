14d71ff8a30e8a31364f3c37083c90d46f22aada
Alireza Bakhshi Zadi Mahmoodi <Alireza.BakhshiZadiMahmoodi@student.oulu.fi>
2022-03-16 21:00:02 +0200

    change offer converter from decorator to a utility function

--- src/resources/converters/offer_converter.py
+++ src/resources/converters/offer_converter.py
@@ -4,32 +4,31 @@ import db.flights
 import db.config
 import datetime
 
-def converter(func):
+def converter(client, origin, destination):
     '''
-    This is to be used as a decorator for each method.
+    This is a convertor for Offer resource to return Python object.
     '''
-    def wrapper(self, client, origin, destination):
-        flights = db.flights.Flight.query.filter_by(
-            origin=origin,
-            destination=destination
-        ).all()
-        if not flights:
-            raise werkzeug.exceptions.NotFound
+    flights = db.flights.Flight.query.filter_by(
+        origin=origin,
+        destination=destination
+    ).all()
+    if not flights:
+        raise werkzeug.exceptions.NotFound
 
-        for f in flights:
-            '''
-            Creates the offers for each flight that matches origin and
-            destination for this client in the db.
-            '''
-            offer = db.offers.Offer(
-                    client=client,
-                    valid_until=datetime.datetime.now(),
-                    flight=f
-                    )
-            db.config.db.session.add(offer)
-        db.config.db.session.commit()
-        offers = db.offers.Offer.query.filter_by(
-                client=client
-                ).all()
-        return func(self, offers)
-    return wrapper
+    for f in flights:
+        '''
+        Creates the offers for each flight that matches origin and
+        destination for this client in the db.
+        '''
+        offer = db.offers.Offer(
+                client=client,
+                valid_until=datetime.datetime.now(),
+                flight=f
+                )
+        db.config.db.session.add(offer)
+    db.config.db.session.commit()
+    offers = db.offers.Offer.query.filter_by(
+            client=client
+            ).all()
+    return offers
+
